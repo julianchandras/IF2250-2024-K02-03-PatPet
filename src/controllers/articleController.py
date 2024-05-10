30ced40fe92dee60204b8eff100cdbe3cf35cc88
ImanuelSG <imanuelgirsang1@gmail.com>
2024-05-10 17:07:51 +0700

    finalize backend for food and pet

--- src/controllers/articleController.py
+++ src/controllers/articleController.py
@@ -1,6 +1,7 @@
-
+from PyQt5.QtWidgets import QStackedWidget
+from models.articleModel import ArticleModel
 class ArticleController:
-    def __init__(self):
+    def __init__(self, stacked_widget : QStackedWidget, article_model : ArticleModel) :
         pass
     
     def getArticle(self, articleId):
